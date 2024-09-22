143ba63505a1553e753c1761bea611602f8ab5cf
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2024-09-22 15:24:34 +0000

    Base With Logger

--- src/main/java/com/todo/todo_st/TodoStApplication.java
+++ src/main/java/com/todo/todo_st/TodoStApplication.java
@@ -1,13 +1,29 @@
 package com.todo.todo_st;
 
+import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodoStApplication {
 
+	private final static Log log = org.apache.commons.logging.LogFactory.getLog(TodoStApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(TodoStApplication.class, args);
+		var welcomeMessage = new WelcomeMessage();
+
+		System.out.println(">>>>>>>>>>>>>>>>>>>>");
+		log.info("Application Started!! --->" + welcomeMessage.getMessage());
+
+		// try (ConfigurableApplicationContext context =
+		// SpringApplication.run(TodoStApplication.class, args)) {
+
+		// var welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
+		// System.out.println(welcomeMessage);
+
+		// }
+
 	}
 
 }
---
+++ src/main/java/com/todo/todo_st/WelcomeMessage.java
@@ -0,0 +1,14 @@
+package com.todo.todo_st;
+
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WelcomeMessage {
+
+  public String getMessage() {
+    return "Hello World!";
+  }
+
+}
